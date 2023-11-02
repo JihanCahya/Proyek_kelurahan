07adf4d65d77e48a6de20a8342b1ead4dbefc2f5
JihanCahya <jihan69cahya@gmail.com>
2023-11-03 06:59:14 +0700

    03/11/2023 - edit dashboard

--- application/views/menu-admin/dashboard.php
+++ application/views/menu-admin/dashboard.php
@@ -3,12 +3,14 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Blank Page</h1>
+                    <h1>Selamat Datang
+                        <?= $user['name']; ?> ||
+                        <?= $user['akses']; ?>
+                    </h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Blank Page</li>
                     </ol>
                 </div>
             </div>
@@ -18,9 +20,7 @@
     <!-- Main content -->
     <section class="content">
         <div class="col-sm-12">
-            <h1>Selamat Datang
-                <?= $user['name']; ?> ||
-                <?= $user['akses']; ?>
+            <h1>
             </h1>
         </div>
     </section>
