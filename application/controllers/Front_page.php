9f06c6746c3e58a754808f3261506d4ea1f8961b
desnathasatria <satriadesnatha@gmail.com>
2023-11-03 09:11:21 +0700

    tambahan_front_page

--- application/controllers/Front_page.php
+++ application/controllers/Front_page.php
@@ -3,7 +3,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Front_page extends CI_Controller
 {
+    var $module_js = ['message'];
+    var $app_data = [];
+    public function header(){
+        $this->load->view('front_page/header', $this->app_data);
+    }
+    public function footer(){
+        $this->load->view('front_page/footer', $this->app_data);
+    }
     public function index(){
-        $this->load->view('front_page/index');
+        $this->header();
+        $this->load->view('front_page/index', $this->app_data);
+        $this->footer();
+    }
+    public function login() {
+        $this->load->view('front_page/auth/login');
     }
 }
