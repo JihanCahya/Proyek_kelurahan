24b301d2521acd673fece7082eda05172ddd566b
JihanCahya <jihan69cahya@gmail.com>
2023-11-05 16:26:30 +0700

    05/11/2023 - Penambahan kelola profil perusahaan

--- application/controllers/Manage_district.php
+++ application/controllers/Manage_district.php
@@ -75,4 +75,87 @@ class Manage_district extends CI_Controller
         $this->load->view('template-admin/end');
         $this->load->view('js-custom', $this->app_data);
     }
+
+    public function get_data()
+    {
+        $result = $this->data->get_all('district_profile')->result();
+        echo json_encode($result);
+    }
+
+    public function edit_data()
+    {
+        $this->form_validation->set_rules('nama', 'Nama', 'required|trim');
+        $this->form_validation->set_rules('alamat', 'Alamat', 'required|trim');
+        $this->form_validation->set_rules('telepon', 'Nomor Telepon', 'required|trim|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('map', 'Link Google Map', 'required|trim|valid_url');
+
+        if ($this->form_validation->run() == false) {
+            $response['errors'] = $this->form_validation->error_array();
+        } else {
+            $where = array('email' => $this->session->userdata('email'));
+            $data['user'] = $this->data->find('st_user', $where)->row_array();
+
+            $id = $this->input->post('id');
+            $nama = $this->input->post('nama');
+            $alamat = $this->input->post('alamat');
+            $telepon = $this->input->post('telepon');
+            $email = $this->input->post('email');
+            $map = $this->input->post('map');
+            $timestamp = $this->db->query("SELECT NOW() as timestamp")->row()->timestamp;
+
+            $data = array(
+                'name' => $nama,
+                'address' => $alamat,
+                'phone_number' => $telepon,
+                'email' => $email,
+                'embed_address' => $map,
+                'updated_date' => $timestamp,
+                'updated_by' => $data['user']['id'],
+            );
+
+            $where = array('id' => $id);
+            $updated = $this->data->update('district_profile', $where, $data);
+
+            if (!$updated) {
+                $response['errors']['database'] = "Failed to update data in the database.";
+            } else {
+                if (!empty($_FILES['image']['name'])) {
+                    $currentDateTime = date('Y-m-d_H-i-s');
+                    $config['upload_path'] = './assets/image/district';
+                    $config['allowed_types'] = 'gif|jpg|jpeg|png';
+                    $config['max_size'] = 2048;
+                    $config['file_name'] = 'district-' . $currentDateTime;
+                    $this->load->library('upload', $config);
+
+                    if ($this->upload->do_upload('image')) {
+                        $upload_data = $this->upload->data();
+                        $logo = $upload_data['file_name'];
+
+                        $data = array('logo' => $logo);
+                        $where = array('id' => $id);
+                        $this->data->update('district_profile', $where, $data);
+                    } else {
+                        $response['errors']['image'] = strip_tags($this->upload->display_errors());
+                    }
+                }
+
+                $response['success'] = "<script>$(document).ready(function () {
+                    var Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 2000,
+                      });
+
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Anda telah melakukan aksi tambah data Data berhasil dimasukkan'
+                      })
+                  });</script>";
+            }
+        }
+
+        echo json_encode($response);
+    }
 }
--- application/views/menu-admin/manage_district.php
+++ application/views/menu-admin/manage_district.php
@@ -16,10 +16,96 @@
     </section>
     <section class="content">
         <div class="card">
-            <h5 class="card-header">Kelola Profil Kelurahan</h5>
+            <h5 class="card-header">Data Profil Kelurahan</h5>
             <div class="card-body">
-
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div id="imagePreview"></div>
+                        <input type="hidden" name="id" class="form-control" value="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="labelImage">Icon Perusahaan</label>
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" name="image" id="image"
+                                    onchange="previewImage(event)">
+                                <label class="custom-file-label" for="card" id="label" name="label">Pilih file</label>
+                                <small class="text-danger pl-1" id="error-image"></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="labelNama">Nama Kelurahan</label>
+                            <input type="text" class="form-control" id="nama" name="nama"
+                                placeholder="Masukkan nama kelurahan">
+                            <small class="text-danger pl-1" id="error-nama"></small>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="labelAlamat">Alamat Kelurahan</label>
+                            <textarea class="form-control" rows="3" id="alamat" name="alamat"
+                                placeholder="Masukkan alamat kelurahan"></textarea>
+                            <small class="text-danger pl-1" id="error-alamat"></small>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="labelTelepon">Nomor Telepon</label>
+                            <input type="number" class="form-control" id="telepon" name="telepon"
+                                placeholder="Masukkan no telepon">
+                            <small class="text-danger pl-1" id="error-telepon"></small>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="labelEmail">Email Kelurahan</label>
+                            <input type="text" class="form-control" id="email" name="email"
+                                placeholder="Masukkan email">
+                            <small class="text-danger pl-1" id="error-email"></small>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="labelMap">Link Map Kelurahan</label>
+                            <textarea class="form-control" rows="5" id="map" name="map"
+                                placeholder="Masukkan map kelurahan"></textarea>
+                            <small class="text-danger pl-1" id="error-map"></small>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="labelPreview">Preview Map</label>
+                        <iframe class="w-100 mb-n2" style="height: 250px;" src="" frameborder="0" allowfullscreen=""
+                            aria-hidden="false" tabindex="0"></iframe>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-2">
+                        <button type="button" id="btn-edit" onclick="edit_data()" class="btn btn-primary">Edit</button>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
 </div>
+
+<script>
+    var base_url = '<?php echo base_url() ?>';
+    var _controller = '<?= $this->router->fetch_class() ?>';
+</script>
