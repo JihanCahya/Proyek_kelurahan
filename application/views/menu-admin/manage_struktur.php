98a855b192cbc40df5580379cdd67eab9eccdf03
JihanCahya <jihan69cahya@gmail.com>
2023-11-06 07:46:57 +0700

    06/11/2023 - Perbaikan tampilan kelola struktur

--- application/views/menu-admin/manage_struktur.php
+++ application/views/menu-admin/manage_struktur.php
@@ -14,148 +14,165 @@
             </div>
         </div>
     </section>
+
     <section class="content">
-        <div class="container-fluid">
-            <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" onclick="submit('tambah')"> <i class="mdi mdi-plus-circle"></i> Input data</button>
-            <hr style="height:2px;border-width:0;color:gray;background-color:gray">
-            <div class="row">
-                <div class="col-sm-3">
-                    <label>Filter Jabatan</label>
-                    <select id="filter" name="filter[]" class="form-control">
-                        <option value="">Semua Jabatan</option>
-                        <?php foreach ($select as $row) : ?>
-                            <option value="<?php echo $row->name_bridge; ?>"><?php echo $row->name_bridge; ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-            </div><br>
-
-            <table id="example" class="table table-hover table-bordered" style="width:100%">
-                <thead class="table-light">
-                    <tr>
-                        <th width="5%">No</th>
-                        <th width="15%">Nama Lengkap</th>
-                        <th width="15%">Jabatan</th>
-                        <th width="25%">NIP</th>
-                        <th width="25%">Foto</th>
-                        <th width="15%">Alamat</th>
-                        <th width="15%">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                </tbody>
-            </table>
-            <div class="modal fade" id="exampleModal">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Form Data Pegawai</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+        <div class="card">
+            <h5 class="card-header">Kelola Pegawai</h5>
+            <div class="card-body">
+                <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"
+                    onclick="submit('tambah')"><i class="fa-solid fa-circle-plus"></i> Input data</button>
+                <hr>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label>Filter jabatan</label>
+                            <select id="filter" name="filter[]" class="form-control akses" style="width: 100%;">
+                                <option value="">Semua Jabatan</option>
+                                <?php foreach ($select as $row): ?>
+                                    <option value="<?php echo $row->name_bridge; ?>">
+                                        <?php echo $row->name_bridge; ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
-                        <div class="modal-body">
+                    </div>
+                </div>
+                <table id="example" class="table table-hover table-bordered" style="width:100%">
+                    <thead class="table-light">
+                        <tr>
+                            <th width="5%">No</th>
+                            <th width="15%">Nama Lengkap</th>
+                            <th width="15%">Jabatan</th>
+                            <th width="25%">NIP</th>
+                            <th width="25%">Foto</th>
+                            <th width="15%">Alamat</th>
+                            <th width="15%">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+</div>
+
+<div class="modal fade" id="exampleModal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Form Data Pegawai</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="form-group">
                             <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <label for="nama" class="col-lg-2 col-form-label">Nama Lengkap</label>
-                                            <div class="col-lg-10">
-                                                <input type="hidden" name="id" class="form-control">
-                                                <input type="text" name="name" id="name" class="form-control" placeholder="Masukkan nama lengkap">
-                                                <small class="text-danger pl-1" id="error-name"></small>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <label for="nama" class="col-lg-2 col-form-label">NIP</label>
-                                            <div class="col-lg-10">
-                                                <input type="hidden" name="id" class="form-control">
-                                                <input type="text" name="NIP" id="NIP" class="form-control" placeholder="Masukkan NIP">
-                                                <small class="text-danger pl-1" id="error-NIP"></small>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <label for="bridge" class="col-lg-2 col-form-label">Jabatan</label>
-                                            <div class="col-lg-10">
-                                                <select id="bridge" name="bridge[]" class="form-control">
-                                                    <?php foreach ($select as $row) : ?>
-                                                        <option value="<?php echo $row->id_bridge; ?>"><?php echo $row->name_bridge; ?>
-                                                        </option>
-                                                    <?php endforeach; ?>
-                                                </select>
-                                                <small class="text-danger pl-3" id="error-bridge"></small>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <label for="image" class="col-lg-2 col-form-label">Foto</label>
-                                            <div class="col-lg-10">
-                                                <div class="input-group">
-                                                    <div class="custom-file">
-                                                        <input type="file" class="custom-file-input" name="image" id="image" onchange="previewImage(event)">
-                                                        <label class="custom-file-label" for="image">Pilih file</label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-lg-4 offset-2">
-                                                <small class="text-danger pl-1" id="error-image"></small>
-                                                <div id="imagePreview"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <label for="nama" class="col-lg-2 col-form-label">Alamat</label>
-                                            <div class="col-lg-10">
-                                                <input type="hidden" name="id" class="form-control">
-                                                <input type="text" name="address" id="address" class="form-control" placeholder="Masukkan Alamat">
-                                                <small class="text-danger pl-1" id="error-address"></small>
-                                            </div>
-                                        </div>
-                                    </div>
-
+                                <label for="nama" class="col-lg-2 col-form-label">Nama Lengkap</label>
+                                <div class="col-lg-10">
+                                    <input type="hidden" name="id" class="form-control">
+                                    <input type="text" name="name" id="name" class="form-control"
+                                        placeholder="Masukkan nama lengkap">
+                                    <small class="text-danger pl-1" id="error-name"></small>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="modal-footer d-flex justify-content-start">
-                            <div class="col-lg-2">
-                                <button type="button" id="btn-tambah" onclick="insert_data()" class="btn btn-outline-primary btn-block">Tambah</button>
-                            </div>
-                            <div class="col-lg-2">
-                                <button type="button" id="btn-ubah" onclick="edit_data()" class="btn btn-outline-primary btn-block">Edit</button>
+                        <div class="form-group">
+                            <div class="row">
+                                <label for="nama" class="col-lg-2 col-form-label">NIP</label>
+                                <div class="col-lg-10">
+                                    <input type="hidden" name="id" class="form-control">
+                                    <input type="text" name="NIP" id="NIP" class="form-control"
+                                        placeholder="Masukkan NIP">
+                                    <small class="text-danger pl-1" id="error-NIP"></small>
+                                </div>
                             </div>
                         </div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <label for="bridge" class="col-lg-2 col-form-label">Jabatan</label>
+                                <div class="col-lg-10">
+                                    <select id="bridge" name="bridge[]" class="form-control bridge"
+                                        style="width: 100%;">
+                                        <?php foreach ($select as $row): ?>
+                                            <option value="<?php echo $row->id_bridge; ?>">
+                                                <?php echo $row->name_bridge; ?>
+                                            </option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                    <small class="text-danger pl-3" id="error-bridge"></small>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <label for="image" class="col-lg-2 col-form-label">Foto</label>
+                                <div class="col-lg-10">
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" name="image" id="image"
+                                                onchange="previewImage(event)">
+                                            <label class="custom-file-label" for="image">Pilih file</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4 offset-2">
+                                    <small class="text-danger pl-1" id="error-image"></small>
+                                    <div id="imagePreview"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <label for="nama" class="col-lg-2 col-form-label">Alamat</label>
+                                <div class="col-lg-10">
+                                    <input type="hidden" name="id" class="form-control">
+                                    <input type="text" name="address" id="address" class="form-control"
+                                        placeholder="Masukkan Alamat">
+                                    <small class="text-danger pl-1" id="error-address"></small>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="modal fade" id="hapusStruktur">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <h5>Klik hapus jika anda ingin menghapus data ini</h5>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <button class="btn btn-warning" type="button" id="btn-hapus" data-dismiss="modal">Hapus</button>
-                    </div>
+
+            <div class="modal-footer d-flex justify-content-start">
+                <div class="col-lg-2">
+                    <button type="button" id="btn-tambah" onclick="insert_data()"
+                        class="btn btn-outline-primary btn-block">Tambah</button>
+                </div>
+                <div class="col-lg-2">
+                    <button type="button" id="btn-ubah" onclick="edit_data()"
+                        class="btn btn-outline-primary btn-block">Edit</button>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</div>
+<div class="modal fade" id="hapusStruktur">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h5>Klik hapus jika anda ingin menghapus data ini</h5>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <button class="btn btn-warning" type="button" id="btn-hapus" data-dismiss="modal">Hapus</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script>
