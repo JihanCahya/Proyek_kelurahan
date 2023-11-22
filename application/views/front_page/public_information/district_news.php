7900820ab204378a462ce8f51b6aced74887d94f
JihanCahya <jihan69cahya@gmail.com>
2023-11-23 06:23:31 +0700

    23/11/2023 - perbaikan

--- application/views/front_page/public_information/district_news.php
+++ application/views/front_page/public_information/district_news.php
@@ -1,19 +1,28 @@
 <body>
-<div class="container mt-5">
-  <div class="row">
-  <?php foreach ($news as $ne): ?>
-    <div class="col-md-6">
-      <div class="card">
-        <img src="<?= base_url('assets/image/news/') . $ne->image ?>" class="card-img-top" alt="Gambar Berita">
-        <div class="card-body">
-          <h5 class="card-title"><?= $ne->title ?></h5>
-          <h6 class="card-title"><?= $ne->sub_title ?></h6>
-          <a href="<?php echo base_url("Front_page/detail_news/$ne->id"); ?>" class="btn btn-primary">Baca Selengkapnya</a>
-        </div>
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">Berita kelurahan</h1>
+    <div class="accordion" id="faqAccordion">
+      <div class="row">
+        <?php foreach ($news as $ne): ?>
+          <div class="col-md-6">
+            <div class="card">
+              <img src="<?= base_url('assets/image/news/') . $ne->image ?>" class="card-img-top" alt="Gambar Berita">
+              <div class="card-body">
+                <h5 class="card-title">
+                  <?= $ne->title ?>
+                </h5>
+                <h6 class="card-title">
+                  <?= $ne->sub_title ?>
+                </h6>
+                <a href="<?php echo base_url("Front_page/detail_news/$ne->id"); ?>" class="btn btn-primary">Baca
+                  Selengkapnya</a>
+              </div>
+            </div>
+          </div>
+        <?php endforeach; ?>
       </div>
     </div>
-    <?php endforeach; ?>
   </div>
-</div>
 </body>
+
 </html>
--- application/views/front_page/public_information/help_information.php
+++ application/views/front_page/public_information/help_information.php
@@ -1,23 +1,26 @@
 <body>
   <div class="container mt-5">
-    <div class="row">
-      <?php foreach ($news as $ne): ?>
-        <div class="col-md-6">
-          <div class="card">
-            <img src="<?= base_url('assets/image/news/') . $ne->image ?>" class="card-img-top" alt="Gambar Bantuan">
-            <div class="card-body">
-              <h5 class="card-title">
-                <?= $ne->title ?>
-              </h5>
-              <h6 class="card-title">
-                <?= $ne->sub_title ?>
-              </h6>
-              <a href="<?php echo base_url("Front_page/detail_information/$ne->id"); ?>" class="btn btn-primary">Baca
-                Selengkapnya</a>
+    <h1 class="text-center mb-4">Informasi Bantuan</h1>
+    <div class="accordion" id="faqAccordion">
+      <div class="row">
+        <?php foreach ($news as $ne): ?>
+          <div class="col-md-6">
+            <div class="card">
+              <img src="<?= base_url('assets/image/news/') . $ne->image ?>" class="card-img-top" alt="Gambar Bantuan">
+              <div class="card-body">
+                <h5 class="card-title">
+                  <?= $ne->title ?>
+                </h5>
+                <h6 class="card-title">
+                  <?= $ne->sub_title ?>
+                </h6>
+                <a href="<?php echo base_url("Front_page/detail_information/$ne->id"); ?>" class="btn btn-primary">Baca
+                  Selengkapnya</a>
+              </div>
             </div>
           </div>
-        </div>
-      <?php endforeach; ?>
+        <?php endforeach; ?>
+      </div>
     </div>
   </div>
 </body>
