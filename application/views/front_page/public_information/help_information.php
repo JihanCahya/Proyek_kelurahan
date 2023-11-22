91a7d27131c48b3e5120afe8851398db7507b59c
JihanCahya <jihan69cahya@gmail.com>
2023-11-23 00:04:51 +0700

    23/11/2023 - perbaikan menu berita user

--- application/views/front_page/public_information/help_information.php
+++ application/views/front_page/public_information/help_information.php
@@ -1,19 +1,25 @@
 <body>
-<div class="container mt-5">
-  <div class="row">
-  <?php foreach ($news as $ne): ?>
-    <div class="col-md-6">
-      <div class="card">
-        <img src="<?= base_url('assets/image/news/') . $ne->image ?>" class="card-img-top" alt="Gambar Bantuan">
-        <div class="card-body">
-          <h5 class="card-title"><?= $ne->title ?></h5>
-          <h6 class="card-title"><?= $ne->sub_title ?></h6>
-          <a href="<?php echo base_url("Front_page/detail_news/$ne->id"); ?>" class="btn btn-primary">Baca Selengkapnya</a>
+  <div class="container mt-5">
+    <div class="row">
+      <?php foreach ($news as $ne): ?>
+        <div class="col-md-6">
+          <div class="card">
+            <img src="<?= base_url('assets/image/news/') . $ne->image ?>" class="card-img-top" alt="Gambar Bantuan">
+            <div class="card-body">
+              <h5 class="card-title">
+                <?= $ne->title ?>
+              </h5>
+              <h6 class="card-title">
+                <?= $ne->sub_title ?>
+              </h6>
+              <a href="<?php echo base_url("Front_page/detail_information/$ne->id"); ?>" class="btn btn-primary">Baca
+                Selengkapnya</a>
+            </div>
+          </div>
         </div>
-      </div>
+      <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
   </div>
-</div>
 </body>
+
 </html>
