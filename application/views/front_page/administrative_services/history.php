a9655ddd92fa6c85deaa9ca8cd3ab14ca173a2e0
JihanCahya <jihan69cahya@gmail.com>
2023-11-22 21:54:45 +0700

    22/11/2023 - Perbaikan history

--- application/views/front_page/administrative_services/history.php
+++ application/views/front_page/administrative_services/history.php
@@ -24,6 +24,63 @@
     </div>
 </div>
 
+<!-- modal untuk lihat detail history transaksi -->
+<div class="modal fade" id="modalDetail">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Detail history pengajuan</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="form-group">
+                            <label for="nama">Nama Pengaju</label>
+                            <div class="input-group">
+                                <input type="hidden" class="form-control" id="id" name="id" readonly>
+                                <input type="text" class="form-control" id="nama" name="nama" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="letter">Nama Surat</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="letter" name="letter" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="date1">Tanggal Pengajuan</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="date1" name="date1" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="date2">Tanggal Selesai</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="date2" name="date2" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="keterangan">Keterangan</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="keterangan" name="keterangan" readonly>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer d-flex justify-content-start">
+                <div class="col-lg-2">
+                    <button type="button" id="btn-tambah" data-dismiss="modal"
+                        class="btn btn-outline-primary btn-block">Tutup</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
     var base_url = '<?php echo base_url() ?>';
     var _controller = '<?= $this->router->fetch_class() ?>';
