e6e27ce71fe904b9f7f6482cd83134c65b9b0c2f
JihanCahya <jihan69cahya@gmail.com>
2023-11-23 07:32:17 +0700

    perbaikan struktur

--- application/views/front_page/district_profile/village_history.php
+++ application/views/front_page/district_profile/village_history.php
@@ -1,43 +1,23 @@
 <!-- ======= About Section ======= -->
 <section id="sejarah" class="sejarah section-bg">
-      <div class="container" data-aos="fade-up">
+  <div class="container" data-aos="fade-up">
 
-        <div class="section-title">
-          <h2>Sejarah Kelurahan</h2>
-          <h3>Kelurahan <span>Burengan</span></h3>
-        </div>
+    <div class="section-title">
+      <h2>Sejarah Kelurahan</h2>
+      <h3>Kelurahan <span>Burengan</span></h3>
+    </div>
 
-        <div class="row">
-          <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
-            <img src="<?= base_url() ?>assets/template-user/img/burengan.jpg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
-            <p class="fst-italic">
-            Burengan adalah sebuah desa di wilayah Kecamatan Pesantren, Kota Kediri, Provinsi Jawa Timur.
-            </p>
-            <ul>
-              <li>
-                <i class="bx bx-store-alt"></i>
-                <div>
-                  <h5>Ullamco laboris nisi ut aliquip consequat</h5>
-                  <p>Magni facilis facilis repellendus cum excepturi quaerat praesentium libre trade</p>
-                </div>
-              </li>
-              <li>
-                <i class="bx bx-images"></i>
-                <div>
-                  <h5>Magnam soluta odio exercitationem reprehenderi</h5>
-                  <p>Quo totam dolorum at pariatur aut distinctio dolorum laudantium illo direna pasata redi</p>
-                </div>
-              </li>
-            </ul>
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </div>
-        </div>
-
+    <div class="row">
+      <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+        <img src="<?= base_url() ?>assets/template-user/img/burengan.jpg" class="img-fluid" alt="">
       </div>
-    </section><!-- End About Section -->
+      <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up"
+        data-aos-delay="100">
+        <p class="fst-italic">
+          Burengan adalah sebuah desa di wilayah Kecamatan Pesantren, Kota Kediri, Provinsi Jawa Timur.
+        </p>
+      </div>
+    </div>
+
+  </div>
+</section><!-- End About Section -->
